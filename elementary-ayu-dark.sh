906b21a7da70e476ca3fe059186736c072b0a1b5
Bastian Schnorbus <basti@ElementaryX1Yoga.fritz.box>
2022-03-11 17:10:36 +0100

    add script to set terminal themes for elementary

---
+++ elementary-ayu-dark.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+# ====================CONFIG THIS =============================== #
+export COLOR_01="#0A0E14"           # HOST
+export COLOR_02="#FF3333"           # SYNTAX_STRING
+export COLOR_03="#C2D94C"           # COMMAND
+export COLOR_04="#FF8F40"           # COMMAND_COLOR2
+export COLOR_05="#59C2FF"           # PATH
+export COLOR_06="#FFEE99"           # SYNTAX_VAR
+export COLOR_07="#95E6CB"           # PROMP
+export COLOR_08="#B3B1AD"           #
+
+export COLOR_09="#4D5566"           #
+export COLOR_10="#FF3333"           # COMMAND_ERROR
+export COLOR_11="#C2D94C"           # EXEC
+export COLOR_12="#FF8F40"           #
+export COLOR_13="#59C2FF"           # FOLDER
+export COLOR_14="#FFEE99"           #
+export COLOR_15="#95E6CB"           #
+export COLOR_16="#B3B1AD"           #
+
+export BACKGROUND_COLOR="#0A0E14"   # Background Color
+export FOREGROUND_COLOR="#B3B1AD"   # Foreground Color (text)
+export CURSOR_COLOR="#E6B450" # Cursor color
+export PROFILE_NAME="Ayu Dark"
+# =============================================================== #
+
+~/dotfiles/elementary-apply-colors.sh
---
+++ elementary-ayu-light.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+# ====================CONFIG THIS =============================== #
+export COLOR_01="#575F66"           # HOST
+export COLOR_02="#F51818"           # SYNTAX_STRING
+export COLOR_03="#86B300"           # COMMAND
+export COLOR_04="#F2AE49"           # COMMAND_COLOR2
+export COLOR_05="#399EE6"           # PATH
+export COLOR_06="#A37ACC"           # SYNTAX_VAR
+export COLOR_07="#4CBF99"           # PROMP
+export COLOR_08="#FAFAFA"           #
+
+export COLOR_09="#8A9199"           #
+export COLOR_10="#F51818"           # COMMAND_ERROR
+export COLOR_11="#86B300"           # EXEC
+export COLOR_12="#F2AE49"           #
+export COLOR_13="#399EE6"           # FOLDER
+export COLOR_14="#A37ACC"           #
+export COLOR_15="#4CBF99"           #
+export COLOR_16="#FAFAFA"           #
+
+export BACKGROUND_COLOR="#FAFAFA"   # Background Color
+export FOREGROUND_COLOR="#575F66"   # Foreground Color (text)
+export CURSOR_COLOR="#FF9940" # Cursor color
+export PROFILE_NAME="Ayu Light"
+# =============================================================== #
+
+~/dotfiles/elementary-apply-colors.sh
