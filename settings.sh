4efcb5d44f85e99ec90a572777e80866ab3e2252
Bastian Schnorbus <basti@ElementaryX1Yoga.fritz.box>
2022-03-13 15:13:35 +0100

    add auto start for virtualenvs when cd'ing to folder

--- settings.sh
+++ settings.sh
@@ -2,7 +2,7 @@
 
 # load fzf key-bindings and completions if installed
 if 
-    which fzf;
+    which fzf > /dev/null;
 then
     source ~/dotfiles/completion.zsh
     source ~/dotfiles/key-bindings.zsh
@@ -10,3 +10,24 @@ fi
 
 # https://stackoverflow.com/questions/53600790/green-background-on-some-directories-oh-my-zsh-zsh
 export LS_COLORS="$LS_COLORS:ow=1;34:tw=1;34:"
+
+
+# automatically start virtualenv if available in folder
+function cd() {
+  builtin cd "$@"
+
+  if [[ -z "$VIRTUAL_ENV" ]] ; then
+    ## If env folder is found then activate the vitualenv
+      if [[ -d ./.venv ]] ; then
+        source ./.venv/bin/activate
+      fi
+  else
+    ## check the current folder belong to earlier VIRTUAL_ENV folder
+    # if yes then do nothing
+    # else deactivate
+      parentdir="$(dirname "$VIRTUAL_ENV")"
+      if [[ "$PWD"/ != "$parentdir"/* ]] ; then
+        deactivate
+      fi
+  fi
+}
